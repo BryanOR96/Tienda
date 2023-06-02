8425e52e311d455f25c08ed1774ddfd808a2096e
BryanOR96 <113558432+BryanOR96@users.noreply.github.com>
2023-06-01 20:11:39 -0600

    semana4
    tarea semana 4 in progress...

---
+++ Semana4/src/main/resources/static/script.js
@@ -0,0 +1,10 @@
+function login(event) {
+  event.preventDefault();
+
+  // Aquí puedes agregar la lógica de autenticación y redirección después del inicio de sesión
+
+  // Redirigir a la página de la tienda
+  window.location.href = "Tienda.info.html";
+}
+
+
